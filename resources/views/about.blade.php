f3b28ec6de9046b10472680d9420fae55e0a7110
ahtesham <ahteshamarain3@gmail.com>
2024-02-06 22:30:47 +0500

    shaheer assignment

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Laravel</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('aboutpage')}}">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('contactpage')}}">Contact us</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('gallerypage')}}">Gallery</a>
+          </li>
+        
+      </ul>
+    </div>
+  </nav>
+  <h1>About page</h1>
